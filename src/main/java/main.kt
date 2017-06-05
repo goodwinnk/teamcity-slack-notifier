f19f61af5e0b9d992e072bc2967d68a9b2b8af04
Nikolay Krasko <goodwinnk@gmail.com>
2017-06-05 03:30:04 +0300

    -- temp: long red event

--- src/main/java/main.kt
+++ src/main/java/main.kt
@@ -7,20 +7,83 @@ import net.gpedro.integrations.slack.SlackApi
 import net.gpedro.integrations.slack.SlackAttachment
 import net.gpedro.integrations.slack.SlackMessage
 import org.jetbrains.teamcity.rest.*
+import java.time.Duration
+import java.time.Instant
+import java.time.LocalDateTime
+import java.util.*
 
 data class Settings(
         val slackWebHookUrl: String, // = "https://hooks.slack.com/services/{icoming-webhook-url}"
+        val slackChannel: String?, // = @nk
+
+        val serverUrl: String = "https://teamcity.jetbrains.com",
+
+        // For status change event
+        val statusChangeEnabled: Boolean = true,
         val number: String = "1.1.4-dev-518",
         val buildConfigurationId: String = "bt345",
         val branches: String = "<default>",
-        val slackChannel: String = "@nk",
-        val serverUrl: String = "https://teamcity.jetbrains.com"
+
+        // For long failed build event
+        val longFailedEnabled: Boolean = true,
+        val triggerAfterDays: Int = 3
 )
 
 fun main(args: Array<String>) {
     val settings = initFromArgs(Settings::class, args)
     println(settings)
 
+    if (settings.longFailedEnabled) {
+        checkBuildLongFailedEvent(settings)
+    }
+
+//    if (settings.statusChangeEnabled) {
+//        checkBuildStatusChangedEvent(settings)
+//    }
+}
+
+private fun checkBuildLongFailedEvent(settings: Settings) {
+    val teamCityInstance = TeamCityInstance.guestAuth(settings.serverUrl)
+    val lastSuccessfulBuild = teamCityInstance
+            .builds()
+            .fromConfiguration(BuildConfigurationId(settings.buildConfigurationId))
+            .withBranch(settings.branches)
+            .withStatus(BuildStatus.SUCCESS)
+            .limitResults(1)
+            .latest() ?: return
+
+//    val finishDate: Instant = lastSuccessfulBuild.fetchFinishDate().toInstant()
+//    val now = LocalDateTime.now()
+//
+//    val daysWithoutSuccessful = Duration.between(now, finishDate).toDays()
+//    if (daysWithoutSuccessful <= settings.triggerAfterDays) {
+//        println("Only $daysWithoutSuccessful days have passed. Will trigger after ${settings.triggerAfterDays} days.")
+//        return
+//    }
+
+    val failedBuilds = teamCityInstance
+            .builds()
+            .fromConfiguration(BuildConfigurationId(settings.buildConfigurationId))
+            .withBranch(settings.branches)
+            .withStatus(BuildStatus.FAILURE)
+            .limitResults(100)
+            .list()
+
+    println("FailedBuilds: ${failedBuilds.size}")
+
+    // TODO: sinceBuild locator
+    val firstFailedBuild = failedBuilds.last { it.id.stringId > lastSuccessfulBuild.id.stringId }
+
+    println("${lastSuccessfulBuild.id.stringId} ${firstFailedBuild.id.stringId}")
+
+    val buildConfiguration = teamCityInstance.buildConfiguration(BuildConfigurationId(settings.buildConfigurationId))
+    println(buildConfiguration.name)
+
+    val slackNotification = firstFailedBuild.createSlackNotification(buildConfiguration)
+    println(slackNotification)
+}
+
+private fun checkBuildStatusChangedEvent(settings: Settings) {
     val teamCityInstance = TeamCityInstance.guestAuth(settings.serverUrl)
     val builds = teamCityInstance
             .builds()
